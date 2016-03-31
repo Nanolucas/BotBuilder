9336f07d2029bf876dc75c1ff5a7996e3ff0599c
Luke D. | Crazy Domains <luke.d@crazydomains.com>
2016-03-31 16:02:33 +0800

    added module system with auto-loading based on commands

--- NanoBot/app.js
+++ NanoBot/app.js
@@ -4,6 +4,7 @@ An awful hello world bot for slack
 
 var Botkit = require('botkit');
 var builder = require('../');
+var bot_name = 'nano';
 
 if (!process.env.token) {
     console.log('Error: Slack token not specified in environment variables!');
@@ -12,23 +13,76 @@ if (!process.env.token) {
 
 var controller = Botkit.slackbot();
 var bot = controller.spawn({
-   token: process.env.token
+	token: process.env.token
 });
 
 var slackBot = new builder.SlackBot(controller, bot);
 slackBot.add('/', function (session) {
-	console.log(session.message);
-	if (/(hi|hello|sup|howdy|hey)/.test(session.message.text)) {
-		session.send('hello human'); 
-	} else if (session.message.text == 'how are you?') {
-		session.send('go fuck yourself'); 
-	}
+	slackBot.handle(session, session.message);
 });
-	
+
+slackBot.handle = function(session, message) {
+	var named_call = false,
+		named_call_regex = new RegExp("^@?" + bot_name + " (.*)", "i");
+
+	if (message.channelData.event == 'direct_message' || named_call_regex.test(message.text)) {
+		named_call = true;
+		console.log('NAMED CALL');
+		message.text = message.text.replace(named_call_regex, "$1");
+	}
+
+	console.log(message.text);
+	if (named_call) {
+		if (named_call && /(hi|hello|sup|howdy|hey)/.test(message.text)) {
+			session.send('hello human');
+		} else if (/\w\s.+/i.test(message.text)) {
+			var potential_module = message.text.replace(/(\w+)\s.+/i, "$1"),
+				potential_command = message.text.replace(/\w+\s(.+)/i, "$1"),
+				fs = require('fs');
+
+				console.log('module: ' + potential_module);
+				console.log('command: ' + potential_command);
+			try {
+				fs.accessSync('./modules/' + potential_module + '.js', fs.F_OK);
+
+				console.log("Module '" + potential_module + "' exists");
+				
+				try {
+					var nanomodule = require('./modules/' + potential_module + '.js');
+					
+					console.log("Module '" + potential_module + "' loaded");
+					
+					try {
+						var nanomodule_instance = new nanomodule();
+						
+						var result = nanomodule_instance.run(potential_command);
+						
+						if (result) {
+							session.send(result);
+						}
+					} catch (e) {
+						console.log("Error: Something broke in module '" + potential_module + "'. \nMessage: " + e);
+					}
+				} catch (e) {
+					console.log("Error: Module '" + potential_module + "' could not be loaded");
+				}
+			} catch (e) {
+				console.log("Error: File './modules/" + potential_module + ".js' not found");
+			}
+		}
+
+		return;
+	}
+
+	if (/(how are you\??)/.test(message.text)) {
+		session.send('go fuck yourself');
+	}
+}
+
 slackBot.listenForMentions();
 
 bot.startRTM(function(err,bot,payload) {
-  if (err) {
-    throw new Error('Could not connect to Slack');
-  }
+	if (err) {
+		throw new Error('Could not connect to Slack');
+	}
 });
---
+++ NanoBot/modules/roll.js
@@ -0,0 +1,9 @@
+"use strict";
+
+class roll {
+	run(command) {
+		return '56';
+	}
+}
+
+module.exports = roll;
