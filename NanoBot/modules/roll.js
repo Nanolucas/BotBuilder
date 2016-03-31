27438a8ad00f7b86694a3d3fcd3adcf88ed9fd6e
Luke D. | Crazy Domains <luke.d@crazydomains.com>
2016-03-31 17:30:51 +0800

    fixed roll module...more

--- NanoBot/modules/roll.js
+++ NanoBot/modules/roll.js
@@ -6,17 +6,17 @@ class roll {
 			values;
 
 		if (/^\d+$/.test(command)) {
-			return '*' + this.random_number(1, command) + '*';
+			return '*' + this.random_number(1, parseInt(command)) + '*';
 		} else if (values = range_regex.exec(command)) {
-			return '*' + this.random_number(values[1], values[2]) + '*';
+			return '*' + this.random_number(parseInt(values[1]), parseInt(values[2])) + '*';
 		}
 
 		return 'I don\'t understand what you said but here is a number from 1-100: *' + this.random_number(1, 100) + '*';
 	}
 
-	random_number(low, high) {
-		console.log('low: ' + low + ', high: ' + high);
-		return Math.floor(Math.random() * high + low);
+	random_number(min, max) {
+		console.log('min: ' + min + ', max: ' + max);
+		return Math.floor(Math.random() * (max - min + 1)) + min;
 	}
 }
 
