01479b204bc210ef3fc813dde18c751b8eeb1010
Luke D. | Crazy Domains <luke.d@crazydomains.com>
2016-04-01 17:15:34 +0800

    added first version of image search via get request

--- NanoBot/modules/image.js
+++ NanoBot/modules/image.js
@@ -1,9 +1,20 @@
 "use strict";
 
+var request = require('request');
+
 class image {
 	run(command) {
+		request('http://www.imtwelve.com/api/image_search.php?search=' + encodeURIComponent(command), function (error, response, body) {
+			if (!response.statusCode == 200) {
+				return 'Error: SOMETHING WENT TERRIBLY WRONG (' + response.statusCode + ')';
+			}
 
-		return 'http://im12.it/g/5028';
+			if (error) {
+				return 'Error: ' . body;
+			}
+
+			return body;
+		})
 	}
 }
 
